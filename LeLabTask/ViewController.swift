e9891d5d732cd25b44ae43fd8ff4b40eae0bf66b
Anees <50396547+artechMobile@users.noreply.github.com>
2021-04-28 14:21:00 +0530

    Initial Commit

---
+++ LeLabTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeLabTask
+//
+//  Created by Anees Mv on 28/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
